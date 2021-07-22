3a9e0be225b0482d95b4aa875e6cc8bd1e23f0b5
David Park <david@howdidothat.info>
2021-07-22 15:01:30 -0400

    Working virtualized display

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useTable, useFilters } from 'react-table';
+import { useTable, useFilters, useBlockLayout } from 'react-table';
 import { useParams } from 'react-router-dom';
+import { FixedSizeList } from 'react-window';
+import styled from 'styled-components';
+import scrollbarWidth from '../scrollbarWidth'
+
+
 import axios from 'axios';
 
 function Leaderboard(props) {
@@ -13,25 +18,94 @@ function Leaderboard(props) {
     const [typeFilter, setTypeFilter] = useState('All');
     // eslint-disable-next-line
     const [nameFilter, setNameFilter] = useState();
-    
+
     // eslint-disable-next-line
     const [tagFilter, setTagFilter] = useState();
-    //const [working, setWorking] = useState(false);
 
-    
-    //const [intialized, setInitialized] = useState(false);
+    const scrollBarSize = React.useMemo(() => scrollbarWidth(), []);
+
+    const Styles = styled.div`
+  padding: 1rem;
+
+  .table {
+    display: inline-block;
+    border-spacing: 0;
+    border: 1px solid black;
+    width: auto;
+    border-right-width: 0;
+
+    .tr {
+      :last-child {
+        .td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    .th,
+    .td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 1px solid black;
+      }
+    }
+
+    .th {
+        font-weight: bold;
+        text-align: center;
+        border-bottom: 1px solid black;
+    }
+  }
+`
+
+const defaultColumn = React.useMemo(
+    () => ({
+      width: 100
+    }),
+    []
+  )
+
 
     const tableInstance = useTable({
-        columns, data
-    }, useFilters);
+        columns, data, defaultColumn
+    }, useBlockLayout, useFilters);
 
     const {
         getTableProps,
         getTableBodyProps,
         headerGroups,
         rows,
+        totalColumnsWidth,
         prepareRow,
     } = tableInstance;
+
+    const RenderRow = React.useCallback(
+        ({ index, style }) => {
+          const row = rows[index]
+          prepareRow(row)
+          return (
+            <div
+              {...row.getRowProps({
+                style,
+              })}
+              className="tr"
+            >
+              {row.cells.map(cell => {
+                return (
+                  <div {...cell.getCellProps()} className="td">
+                    {cell.render('Cell')}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        },
+        [prepareRow, rows]
+      )
 
     /* load the results */
     useEffect(() => {
@@ -40,39 +114,47 @@ function Leaderboard(props) {
         axios.get(`https://4chbxgj610.execute-api.us-east-1.amazonaws.com/production/${eventid}`)
             .then(res => {
                 let cols = [
-                    {
-                        Header: 'Overall Rank',
-                        accessor: 'rank',
-                    },
-                    /*
-                    {
-                        Header: 'Pair/Individual Rank',
-                        accessor: 'typerank'
-                    },
-                    */
-                    {
-                        Header: 'Name',
-                        accessor: 'display_name'
-                    },
-                    {
-                        Header: 'Division',
-                        accessor: 'division'
-                    },
-                    {
-                        Header: 'Overall Score',
-                        accessor: 'totalScore'
-                    },
-                    {
-                        Header: 'Type',
-                        accessor: 'entry_type'
-                    },
-                    {
-                        Header: 'Clean',
-                        id: 'clean',
-                        accessor: (row) => {
-                            return row.clean && Object.keys(row.solves).length ? '*' : ''
+                    {Header: '',
+                     id: 'empty',
+                    columns: [
+                        {
+                            Header: 'Overall Rank',
+                            accessor: 'rank',
+                            width: 70
+                        },
+                        /*
+                        {
+                            Header: 'Pair/Individual Rank',
+                            accessor: 'typerank'
+                        },
+                        */
+                        {
+                            Header: 'Name',
+                            accessor: 'display_name',
+                            width: 300
+                        },
+                        {
+                            Header: 'Division',
+                            accessor: 'division'
+                        },
+                        {
+                            Header: 'Overall Score',
+                            accessor: 'totalScore'
+                        },
+                        {
+                            Header: 'Type',
+                            accessor: 'entry_type'
+                        },
+                        {
+                            Header: '',
+                            id: 'clean',
+                            accessor: (row) => {
+                                return row.clean && Object.keys(row.solves).length ? '*' : ''
+                            },
+                            width: 25
                         }
-                    }
+    
+                    ]}
                 ];
                 /* column group for each puzzle */
                 res.data.event.puzzles.forEach(v => {
@@ -81,22 +163,26 @@ function Leaderboard(props) {
                         columns: [
                             {
                                 Header: 'Score',
-                                accessor: `solves.${v.id}.score`
+                                accessor: `solves.${v.id}.score`,
+                                width: 70
                             },
                             {
                                 Header: 'Time',
-                                accessor: `solves.${v.id}.time`
+                                accessor: `solves.${v.id}.time`,
+                                width: 70
+
                             },
                             {
                                 Header: 'Correct',
-                                accessor: `solves.${v.id}.correct`
+                                accessor: `solves.${v.id}.correct`,
+                                width: 70
                             },
-                            
+
                         ]
                     })
                 })
                 setEvent(res.data.event);
-                
+
                 setStandings(res.data.entries);
                 /*
                 setStandings(res.data.entries.map((v, i) => {
@@ -112,7 +198,7 @@ function Leaderboard(props) {
                 */
                 setColumns(cols);
                 if (res.data.event && res.data.event.divisions && res.data.event.divisions.length > 1) tableInstance.setFilter('division', res.data.event.divisions[0]);
-                document.title = `Boswords: ${res.data.event.Title} Standings`; 
+                document.title = `Boswords: ${res.data.event.Title} Standings`;
             })
 
 
@@ -137,73 +223,65 @@ function Leaderboard(props) {
             {
                 event &&
                 <div>
-                    <h1>Leaderboard</h1>
                     <h2>{event.Title}</h2>
                     <div className="filters">
                         {
-                            event.divisions && event.divisions.length > 1 && 
+                            event.divisions && event.divisions.length > 1 &&
                             <div>
                                 <label className="form-label">Choose a Division:</label>
-                                <select className="form-control"name="divs" onChange={handleDivChange}>
-                                    { event.divisions.map((v) => {
+                                <select className="form-control" name="divs" onChange={handleDivChange}>
+                                    {event.divisions.map((v) => {
                                         return <option key={v} value={v}>{v}</option>
                                     })
                                     }
                                 </select>
                             </div>
                         }
-                        <label className="form-label">Filter by Entry Type:</label>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="typef" value="All" onChange={handleTypeChange} checked={typeFilter === 'All'} id="allradio" /> 
+                        <p className="form-label">Filter by Entry Type:</p>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="typef" value="All" onChange={handleTypeChange} checked={typeFilter === 'All'} id="allradio" />
                             <label className="form-check-label" htmlFor="allradio">All</label>
                         </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="typef" value="Individual" onChange={handleTypeChange} checked={typeFilter === 'Individual'} id="indradio" /> 
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="typef" value="Individual" onChange={handleTypeChange} checked={typeFilter === 'Individual'} id="indradio" />
                             <label className="form-check-label" htmlFor="indradio">Individuals</label>
                         </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="radio" name="typef" value="Pair" onChange={handleTypeChange} checked={typeFilter === 'Pair'} id="pradio" /> 
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="typef" value="Pair" onChange={handleTypeChange} checked={typeFilter === 'Pair'} id="pradio" />
                             <label className="form-check-label" htmlFor="pradio">Pairs</label>
                         </div>
                         <div>
-                            <label className="form-label">Filter by name:</label> 
-                            <input type="text" name="namefilter" onChange={handleNameChange} className="form-control" />
+                            <label className="form-label">Filter by name:</label>
+                            <input type="text" name="namefilter" value={nameFilter} onChange={handleNameChange} className="form-control" />
                         </div>
-                        
+
                     </div>
-                    <table {...getTableProps} className="table">
-                        <thead>
+                    <Styles>
+                    <div {...getTableProps()} className="table">
+                        <div>
                             {headerGroups.map(headerGroup => (
-                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                <div {...headerGroup.getHeaderGroupProps()} className="tr">
                                     {headerGroup.headers.map(column => (
-                                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                        <div {...column.getHeaderProps()} className="th">
+                                            {column.render('Header')}
+                                        </div>
                                     ))}
-                                </tr>
+                                </div>
                             ))}
-                        </thead>
-                        <tbody {...getTableBodyProps()}>
-                            {// Loop over the table rows
-                                rows.map(row => {
-                                    // Prepare the row for display
-                                    prepareRow(row)
-                                    return (
-                                        // Apply the row props
-                                        <tr {...row.getRowProps()}>
-                                            {// Loop over the rows cells
-                                                row.cells.map(cell => {
-                                                    // Apply the cell props
-                                                    return (
-                                                        <td {...cell.getCellProps()}>
-                                                            {// Render the cell contents
-                                                                cell.render('Cell')}
-                                                        </td>
-                                                    )
-                                                })}
-                                        </tr>
-                                    )
-                                })}
-                        </tbody>
-                    </table>
+                        </div>
+
+                        <div {...getTableBodyProps()}>
+                            <FixedSizeList
+                                height={400}
+                                itemCount={rows.length}
+                                itemSize={35}
+                                width={totalColumnsWidth + scrollBarSize}
+                            >
+                                {RenderRow}
+                            </FixedSizeList>
+                        </div>
+                    </div>
+                    </Styles>
                 </div>
 
             }
